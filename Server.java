1b9eb45ee9398bd72fb1ba537e0803cbc5cbacc8
Aleksandar995 <63589411+Aleksandar995@users.noreply.github.com>
2020-04-23 19:22:11 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -9,18 +9,19 @@ import java.util.Set;
 public class Server {
 
     public static int port = 1234;
-    private static Random random = new Random();
+   /* private static Random random = new Random();
     //Ovo treba uopstit u niz... I sad uvek pokrece za Igru sa kartama prva, druga, treca, to treba da se uopsti da pokrece sa 3 iz niza
     private static int prva = random.nextInt(101);
     private static int druga = random.nextInt(101);
     private static int treca = random.nextInt(101);
     private static int cetvrta = random.nextInt(101);
     private static int peta = random.nextInt(101);
-    private static int sesta = random.nextInt(101);
+    private static int sesta = random.nextInt(101);*/
+    static int []niz = RandomNumbers();
 
 
 // (2) some time later ... assign elements to the array
-//    int []niz= RandomNumbers();
+
 
 
     public static void main(String[] args) {
@@ -32,15 +33,13 @@ public class Server {
             Scanner s = new Scanner(System.in);
             int broj_igraca = s.nextInt();
             int brojac = 0;
+            int brojac_niza = 0;
 
 
             while (brojac < broj_igraca) {
                 Socket klijent = server.accept();
                 System.out.println("Prihvacen klijent! Broj igraca je " + ++brojac);
-                new Thread(new KlijentHandlerRinnable(klijent, prva, druga, treca)).start();
-                // uvedemo novu promenljivu int indeks, koji je u startu 0, tj na pocetku niza
-                // kao argumente za KlijentHandlerRunnable prosledimo indeks, indeks+1, indeks+2 tj prva 3 iz niza
-                //posle toga samo pomerimo taj indeks kako ne bi ponovo koristio iste brojeve
+                new Thread(new KlijentHandlerRinnable(klijent, niz[brojac_niza++], niz[brojac_niza++], niz[brojac_niza++])).start();
 
 
             }//end while
@@ -49,10 +48,6 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public  int getPrva() {
-        return prva;
     }
 
     public static int[] RandomNumbers(){
@@ -72,7 +67,7 @@ public class Server {
             used.add(newRandom);
         }
         return result;
-    }//end metode random numbers
+    }
 
 
 }
