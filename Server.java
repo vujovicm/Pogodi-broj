3c09e6da388eaed0e5ec101b776ca3095edfe4ef
vujovicm <63158265+vujovicm@users.noreply.github.com>
2020-04-23 11:34:29 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Server {
 
@@ -18,7 +20,7 @@ public class Server {
 
 
 // (2) some time later ... assign elements to the array
-
+//    int []niz= RandomNumbers();
 
 
     public static void main(String[] args) {
@@ -36,6 +38,9 @@ public class Server {
                 Socket klijent = server.accept();
                 System.out.println("Prihvacen klijent! Broj igraca je " + ++brojac);
                 new Thread(new KlijentHandlerRinnable(klijent, prva, druga, treca)).start();
+                // uvedemo novu promenljivu int indeks, koji je u startu 0, tj na pocetku niza
+                // kao argumente za KlijentHandlerRunnable prosledimo indeks, indeks+1, indeks+2 tj prva 3 iz niza
+                //posle toga samo pomerimo taj indeks kako ne bi ponovo koristio iste brojeve
 
 
             }//end while
@@ -49,6 +54,25 @@ public class Server {
     public  int getPrva() {
         return prva;
     }
+
+    public static int[] RandomNumbers(){
+        //redja brojeve iz (0,100] na random nacin u niz
+
+        final Random r = new Random();
+        int[] result = new int[100];
+        Set<Integer> used = new HashSet<Integer>();
+
+        for (int i = 0; i < 100; i++) {
+
+            int newRandom;
+            do {
+                newRandom = r.nextInt(100)+1; //ovo +1 je da ne bude ukljucena 0
+            } while (used.contains(newRandom));
+            result[i] = newRandom;
+            used.add(newRandom);
+        }
+        return result;
+    }//end metode random numbers
 
 
 }
